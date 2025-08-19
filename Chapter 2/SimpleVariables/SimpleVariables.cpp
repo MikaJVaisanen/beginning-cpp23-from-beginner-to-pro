0c8f5cd1986209dd2ce472d27e97195671327f31
Mika Väisänen <mika.juhani.vaisanen@icloud.com>
2025-08-19 19:49:10 +0300

    A console program that creates a few variables and outputs them

---
+++ Chapter 2/SimpleVariables/SimpleVariables.cpp
@@ -0,0 +1,13 @@
+// SimpleVariables.cpp - Writing values of variables to the screen
+import std;
+
+int main()
+{
+	int apple_count{ 15 };								// Number of apples
+	int orange_count{ 5 };								// Number of oranges
+	int total_fruit{ apple_count + orange_count };		// Total number of fruit
+
+	std::println("The value of apple_count is {}", apple_count);
+	std::println("The value of orange_count is {}", orange_count);
+	std::println("The value of total_fruit is {}", total_fruit);
+}
