9d5eeb0dbf9cc2f786ece0c1d70818519ae10a8a
Mika Väisänen <mika.juhani.vaisanen@icloud.com>
2025-08-20 18:42:40 +0300

    A console program that outputs the limits of fundamental data types

---
+++ Chapter 2/TypeLimits/TypeLimits.cpp
@@ -0,0 +1,18 @@
+// TypeLimits.cpp - Finding maximum and minimum values for data types
+import std;
+
+int main()
+{
+	std::println("The range for type short is from {} to {}",
+		std::numeric_limits<short>::min(), std::numeric_limits<short>::max());
+	std::println("The range for type unsigned int is from {} to {}",
+		std::numeric_limits<unsigned int>::min(), std::numeric_limits<unsigned int>::max());
+	std::println("The range for type long long is from {} to {}",
+		std::numeric_limits<long long>::min(), std::numeric_limits<long long>::max());
+	std::println("The positive range for type float is from {} to {}",
+		std::numeric_limits<float>::min(), std::numeric_limits<float>::max());
+	std::println("The full range for type float is from {} to {}",
+		std::numeric_limits<float>::lowest(), std::numeric_limits<float>::max());
+	std::println("The positive range for type double is from {} to {}",
+		std::numeric_limits<double>::min(), std::numeric_limits<double>::max());
+}
